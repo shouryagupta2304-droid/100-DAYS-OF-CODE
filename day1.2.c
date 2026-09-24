bd90cbf61a5c50504bea62e672961d43a0d6a1de
SHOURYA's GIT <shouryagupta2304@gmail.com>
2026-09-24 23:11:23 +0530

    100days

--- day1.2.c
+++ day1.2.c
@@ -1,16 +1,19 @@
 // sum,difference,product and quotient of two numbers //
 #include<stdio.h>
 int main(){
-    int a,b,sum ,difference,product, quotient;
+    int a,b,sum ,difference,product;
+    float quotient;
     printf("Enter the numbers:");
     scanf("%d %d",&a, &b);
     sum = a+b;
     difference = a-b;
     product = a*b;
     quotient = a/b;
+    b !=0;
     printf("The sum of %d and %d is:%d\n",a,b,sum);
     printf("The difference of %d and %d is:%d\n",a,b,difference);
     printf("The product of %d and %d is:%d\n",a,b,product);
     printf("The quotient of %d and %d is:%d\n",a,b,quotient);
     return 0;
+
 }
